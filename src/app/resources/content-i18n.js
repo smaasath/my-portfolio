e35ed898fb635bc45cf4d6fabeb2e1e211206874
smaasath <aasadh2000@gmail.com>
2025-03-09 06:35:00 +0530

    name change

--- src/app/resources/content-i18n.js
+++ src/app/resources/content-i18n.js
@@ -2,7 +2,7 @@ import { InlineCode } from "@/once-ui/components";
 
 const createI18nContent = (t) => {
     const person = {
-        firstName: 'Selene',
+        firstName: 'Aasath',
         lastName:  'Yu',
         get name() {
             return `${this.firstName} ${this.lastName}`;
